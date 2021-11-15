845fc7dfbada9532dd9ea8b0b472abc05de23d83
Arup Mandal <40239193+arupmandal@users.noreply.github.com>
2021-11-15 10:16:20 +0530

    Update repositories.js

--- src/data/repositories.js
+++ src/data/repositories.js
@@ -4,8 +4,8 @@ export const repos = [
     description:
       'My personal portfolio to show my skills, experience, and articles I published.',
     languages: 'Typescript, React, ChakraUi',
-    url: 'https://github.com/MA-Ahmad/portfolio',
-    live: 'https://mahmad.me',
+    url: 'https://github.com/arupmandal/arupmandal.github.io',
+    live: 'https://arupmandal.me',
     stars: '60',
     fork: '10'
   },
@@ -14,67 +14,11 @@ export const repos = [
     description:
       'This portfolio built by using Next.js, ChakraUi and Typescript.',
     languages: 'Nextjs, chakraUi',
-    url: 'https://github.com/MA-Ahmad/myPortfolio',
-    live: 'https://my-portfolio-ma-ahmad.vercel.app/',
+    url: 'https://github.com/arupmandal/myPortfolio2',
+    live: '',
     stars: '61',
     fork: '14'
   },
-  {
-    title: 'Notebook',
-    description: 'Make notes for your daily work.',
-    languages: 'Typescript, React, ChakraUi',
-    url: 'https://github.com/MA-Ahmad/notebook',
-    live: 'https://ma-ahmad.github.io/notebook',
-    stars: '17',
-    fork: '7'
-  },
-  {
-    title: 'Blog App(React)',
-    description:
-      'A React Blog App built by using libraries like ChakraUI, Formik.',
-    languages: 'Javascript, React, ChakraUi, Formik',
-    url: 'https://github.com/MA-Ahmad/reactBlog',
-    live: 'https://ma-ahmad.github.io/reactBlog',
-    stars: '13',
-    fork: '2'
-  },
-  {
-    title: 'Blog App (Front-end)',
-    description: 'React frontend of a blog app with rails on backend.',
-    languages: 'Javascript, React, ChakraUi',
-    url: 'https://github.com/MA-Ahmad/blog-app-react-frontend',
-    live: 'https://blog-frontend-react.herokuapp.com',
-    stars: '10',
-    fork: '1'
-  },
-  {
-    title: 'Blog App',
-    description: 'A simple react+rails(RR) CRUD app with tailwindcss.',
-    languages: 'Ruby, Rails, Javascript, React, TailwindCss',
-    url: 'https://github.com/MA-Ahmad/react_rails_blog',
-    live: 'https://react-on-rails-blog.herokuapp.com',
-    stars: '4'
-  },
-  {
-    title: 'Video-to-text',
-    description: 'Convert a video to text using ruby.',
-    languages: 'Ruby',
-    url: 'https://github.com/MA-Ahmad/Video-to-text',
-    stars: '3'
-  },
-  {
-    title: 'Blog App (Back-end)',
-    description: 'Rails backend api for blog app with react on frontend.',
-    languages: 'Ruby, Rails',
-    url: 'https://github.com/MA-Ahmad/blog-app-rails-backend',
-    stars: '3'
-  },
-  {
-    title: 'Authentication API',
-    description:
-      'Simple Authentication API that can be used with any front-end app like React, Vue, Angular app.',
-    languages: 'Ruby, Rails',
-    url: 'https://github.com/MA-Ahmad/rails-authentication-app',
-    stars: '2'
-  }
-];
+ 
+    
+  
